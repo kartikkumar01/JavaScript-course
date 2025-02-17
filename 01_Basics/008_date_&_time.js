d392f1c5136ad99ebb194bc3558877ee7ed9944a
Kartik Kumar <kartikjhaba@gmail.com>
2025-02-17 18:05:04 +0530

    Create 008_date_&_time.js

---
+++ 01_Basics/008_date_&_time.js
@@ -0,0 +1,11 @@
+//Date object is complex in js and it is shifting to Temporal object
+
+let date = new Date()
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+console.log(date.toTimeString());
