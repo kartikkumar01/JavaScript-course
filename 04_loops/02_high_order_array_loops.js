20e5857a2f7d7c1def2b2e538f16f26913c9ef44
Kartik Kumar <kartikjhaba@gmail.com>
2025-02-23 13:58:28 +0000

    high order loops done

---
+++ 04_loops/02_high_order_array_loops.js
@@ -0,0 +1,4 @@
+// for each do not return any values explicitly or implicitly
+// so we can use filter which return array while checking condition
+// map har ek value pe operation perform karta hai or har value return karta hai
+// reduce shopping cart me kaafi use hota hai jaha hame bill ka total nikalna hota hai
